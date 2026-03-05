adbf2f9a91da3c0322cd04c257997db09a278992
Raja Sherina T <rajasherinaa@gmail.com>
2026-03-05 23:41:30 +0530

    Added simple calculator python program

--- simple_calculator.py
+++ simple_calculator.py
@@ -1,4 +1,25 @@
-a=int(input("Enter the value of a: "))
-b=int(input("Enter the value of b: "))
-sum=a+b
-print("The sum of a and b is: ",sum)
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("Choose operation:")
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+
+choice = input("Enter choice (1/2/3/4): ")
+
+if choice == "1":
+    print("Result =", num1 + num2)
+
+elif choice == "2":
+    print("Result =", num1 - num2)
+
+elif choice == "3":
+    print("Result =", num1 * num2)
+
+elif choice == "4":
+    print("Result =", num1 / num2)
+
+else:
+    print("Invalid choice")
