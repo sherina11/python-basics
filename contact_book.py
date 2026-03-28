0b71c49fae8cf3257e05d44866ad60f7bdc4536f
Raja Sherina T <rajasherinaa@gmail.com>
2026-03-28 21:52:28 +0530

    Added contact book mini project in python

---
+++ contact_book.py
@@ -0,0 +1,29 @@
+contacts = {}
+
+while True:
+    print("\n1. Add Contact")
+    print("2. View Contacts")
+    print("3. Exit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        name = input("Enter name: ")
+        phone = input("Enter phone number: ")
+        contacts[name] = phone
+        print("Contact added successfully!")
+
+    elif choice == "2":
+        if contacts:
+            print("\nContacts List:")
+            for name, phone in contacts.items():
+                print(name, ":", phone)
+        else:
+            print("No contacts found!")
+
+    elif choice == "3":
+        print("Exiting program...")
+        break
+
+    else:
+        print("Invalid choice! Please try again.")
